bb07b7fc8a0fcf9c9bbdc9c6defbf9691f69b3da
Sulay-Varonis <sulayman.bah028@gmail.com>
2024-06-10 15:07:37 -0400

    Enemy updates

--- enemy.py
+++ enemy.py
@@ -2,13 +2,15 @@ import pygame
 import os       
 from pathlib import Path    
 import constants as c
+from pygame.math import Vector2
 
 class Enemy(pygame.sprite.Sprite):
     def __init__(self,waypoints):
         self.waypoints = waypoints
         pygame.sprite.Sprite.__init__(self)
         image = pygame.image.load("C:/Users/sbah/Sulay_TD/Sulay-s-Tower-Defense/sprites/mgMan.png")
-        self.pos = self.waypoints[0]
+        self.pos = Vector2(self.waypoints[0])
+        self.target_waypoint = 1
         self.image = pygame.transform.scale(image, (60,60))
         self.rect = self.image.get_rect()
         #self.rect.fit(5,5,5,5)
@@ -19,5 +21,8 @@ class Enemy(pygame.sprite.Sprite):
         self.move(self.waypoints)
 
     def move(self,waypoints):
-        count=0
-        self.rect.x=waypoints[count+1][0]
+        self.target = Vector2(self.waypoints[self.target_waypoint])
+        self.movement = self.target-self.pos
+        self.pos += self.movement.normalize()
+        self.rect.center=self.pos
+        self.rect.x+=1
