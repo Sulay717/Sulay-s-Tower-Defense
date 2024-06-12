a89e0b2a2bc3878e14a4637eef0a12caa43ddf04
Sulay-Varonis <sulayman.bah028@gmail.com>
2024-06-12 14:41:45 -0400

    turret

--- turret.py
+++ turret.py
@@ -1,5 +1,9 @@
 import pygame
 
-class Turret():
+class Turret(pygame.sprite.Sprite()):
     def __init__(self,image,pos):
-        pass
+        pygame.sprite.Sprite.__init__(self)
+        self.image = image
+        self.pos = pos
+        self.rect.center = pos
+        
